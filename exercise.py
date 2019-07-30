805a1e92727d725a9d5da2d47017c95304802a9a
EY-X <e.yosefx128@gmail.com>
2019-07-30 01:21:03 -0400

    First Commit

---
+++ exercise.py
@@ -0,0 +1,20 @@
+def percent_to_grade(percentage):
+    percent = input(percentage)
+    if percentage >= 90 and percentage <= 100:
+        return "A+"
+    elif percentage >= 80 and percentage < 90:
+        return "A"   
+    elif percentage >= 70 and percentage < 80:
+        return "A-"
+    elif percentage >= 65 and percentage < 70:
+        return "B+"
+    elif percentage >= 60 and percentage < 65:
+        return "B"   
+    elif percentage >= 50 and percentage < 60:
+        return "C"   
+    elif percentage >= 45 and percentage < 50:
+        return "D"   
+    elif percentage >= 40 and percentage < 45:
+        return "E"   
+    else:
+        return "F"
